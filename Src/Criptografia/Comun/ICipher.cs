b598fa105ef0f76db875a9e4843859c7e4c5bb32
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-16 20:33:58 -0500

    Interface general para encriptar y desencriptar

---
+++ Src/Criptografia/Comun/ICipher.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgramacionDiscreta.Src.Criptografia.Comun
+{
+    public interface ICipher
+    {
+        string Encrypt(string text, int key);
+        string Decrypt(string text, int key);
+    }
+}

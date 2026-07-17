c33b8c751be2ea85cea2b2af7dc24631c2567a2e
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-16 20:34:47 -0500

    Clase para la normalización de las cadenas en mayusculas y sin tildes

---
+++ Src/Criptografia/Comun/TextNormalizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace ProgramacionDiscreta.Src.Criptografia.Comun
+{
+    // Transforma el texto ingresado para tenerlo siempre en mayusculas sin tildes
+    public static class TextNormalizer
+    {
+        public static string Normalize(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return string.Empty;
+
+            string normalized = text.Normalize(NormalizationForm.FormD);
+
+            StringBuilder builder = new();
+
+            foreach (char c in normalized)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                {
+                    builder.Append(c);
+                }
+            }
+
+            return builder.ToString().ToUpper();
+        }
+    }
+}

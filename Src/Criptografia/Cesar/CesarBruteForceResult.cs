6113cfad4d1abfb96ab02ed87954d16318f64af3
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-16 20:36:30 -0500

    Creada la clase para mostrar todas las soluciones del encriptado de Cesar

---
+++ Src/Criptografia/Cesar/CesarBruteForceResult.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgramacionDiscreta.Src.Criptografia.Cesar
+{
+    public class CesarBruteForceResult
+    {
+        public int Shift { get; set; }
+        public string Text { get; set; } = string.Empty;
+    }
+}

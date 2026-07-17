00e887f447c627744c7b70eb5448889b826b613b
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-16 20:33:31 -0500

    Clase de alfabeto latino para usar en todo el apartado de criptografia

---
+++ Src/Criptografia/Comun/Alfabeto.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgramacionDiscreta.Src.Criptografia.Comun
+{
+    public class Alfabeto
+    {
+        public const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static int Length => Letters.Length;
+    }
+}

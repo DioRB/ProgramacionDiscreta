3723f985150cc881179e384ab83b7d91110c4b66
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-17 22:42:48 -0500

    Funciones de encriptación y desenciptación para el RSA

---
+++ Src/Criptografia/RSA/AritmeticaModularService.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace ProgramacionDiscreta.Src.Criptografia.RSA
+{
+    public class AritmeticaModularService
+    {
+        // La encriptación y desencriptación usando el metodo ModPow de BigInteger
+        public BigInteger Encrypt(BigInteger message, BigInteger e, BigInteger n)
+        {
+            return BigInteger.ModPow(message, e, n);
+        }
+
+        public BigInteger Decrypt(BigInteger cipher, BigInteger d, BigInteger n)
+        {
+            return BigInteger.ModPow(cipher, d, n);
+        }
+    }
+}

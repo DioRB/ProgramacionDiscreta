9936b74a2fdf3fcc0cc570198ee5ad5cfae0c3d2
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-17 22:52:13 -0500

    Obtiene las partes y las reconstruye para cada servidor

---
+++ Src/Criptografia/MPC/SecretSharing.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+namespace ProgramacionDiscreta.Src.Criptografia.MPC
+{
+    public class SecretSharing
+    {
+        private readonly Random _random = new();
+        
+        // Divide la nota en dos partes aleatorias y una tercera por la resta.
+        public NotaSModel SplitSecret(int value, BigInteger modulus)
+        {
+            BigInteger share1 = _random.Next(0, (int)modulus);
+            BigInteger share2 = _random.Next(0, (int)modulus);
+
+            BigInteger share3 =
+                Mod(value - share1 - share2, modulus);
+
+            return new NotaSModel
+            {
+                OriginalValue = value,
+                Share1 = share1,
+                Share2 = share2,
+                Share3 = share3
+            };
+        }
+
+        public BigInteger Reconstruct(
+            BigInteger share1,
+            BigInteger share2,
+            BigInteger share3,
+            BigInteger modulus)
+        {
+            // Reconstruye sumando las partes usando el modulo
+            return Mod(share1 + share2 + share3, modulus);
+        }
+        //Modulo
+        private BigInteger Mod(BigInteger value, BigInteger modulus)
+        {
+            return ((value % modulus) + modulus) % modulus;
+        }
+    }
+}

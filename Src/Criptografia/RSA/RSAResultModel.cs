0d9830a5314ed29f8bca33e74748b91e0daa7618
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-17 22:44:28 -0500

    Modelo para los datos

---
+++ Src/Criptografia/RSA/RSAResultModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace ProgramacionDiscreta.Src.Criptografia.RSA
+{
+    public class RSAResultModel
+    {
+        public bool IsValid { get; set; }
+
+        public string ValidationMessage { get; set; } = "";
+
+        public BigInteger P { get; set; }
+
+        public BigInteger Q { get; set; }
+
+        public BigInteger E { get; set; }
+
+        public BigInteger D { get; set; }
+
+        public BigInteger N { get; set; }
+
+        public BigInteger Phi { get; set; }
+
+        public BigInteger Message { get; set; }
+
+        public BigInteger Cipher { get; set; }
+
+        public BigInteger DecryptedMessage { get; set; }
+    }
+}

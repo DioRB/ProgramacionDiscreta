c7d60227a4bd267bc06f5aaf8d2f5b78a98a9677
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-17 22:50:28 -0500

    Proceso general y cálculo haciendo uso de las funciones

---
+++ Src/Criptografia/MPC/MPC.cs
@@ -0,0 +1,51 @@
+﻿using System.Numerics;
+
+namespace ProgramacionDiscreta.Src.Criptografia.MPC
+{
+    public class MPC
+    {
+        private readonly SecretSharing _sharingService;
+
+        //Modulo grande
+        private readonly BigInteger _modulus = 1510841;
+
+        public MPC(SecretSharing sharingService)
+        {
+            _sharingService = sharingService;
+        }
+        // Función del proceso de suma por servidores y reconstrucción
+        public MPCResultModel Process(List<int> grades)
+        {
+            MPCResultModel result = new();
+
+            foreach (int grade in grades)
+            {
+                NotaSModel share =
+                    _sharingService.SplitSecret(
+                        grade,
+                        _modulus);
+                //Recolecta todas las notas que le corresponde a cada servidor
+                result.Shares.Add(share);
+
+                result.Server1Total += share.Share1;
+                result.Server2Total += share.Share2;
+                result.Server3Total += share.Share3;
+            }
+
+            result.Total =
+                _sharingService.Reconstruct(
+                    result.Server1Total,
+                    result.Server2Total,
+                    result.Server3Total,
+                    _modulus);
+
+            if (grades.Count > 0)
+            {
+                result.Average =
+                    (double)result.Total / grades.Count;
+            }
+
+            return result;
+        }
+    }
+}

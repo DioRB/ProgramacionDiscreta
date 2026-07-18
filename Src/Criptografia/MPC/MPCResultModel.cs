b0938a5cf0b342560e5d6ebebd20f53792d3567b
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-17 22:51:13 -0500

    Modelo del total de notas, servidores, resultado y promedio

---
+++ Src/Criptografia/MPC/MPCResultModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace ProgramacionDiscreta.Src.Criptografia.MPC
+{
+    public class MPCResultModel
+    {
+        public List<NotaSModel> Shares { get; set; } = new();
+
+        public BigInteger Server1Total { get; set; }
+
+        public BigInteger Server2Total { get; set; }
+
+        public BigInteger Server3Total { get; set; }
+
+        public BigInteger Total { get; set; }
+
+        public double Average { get; set; }
+    }
+}

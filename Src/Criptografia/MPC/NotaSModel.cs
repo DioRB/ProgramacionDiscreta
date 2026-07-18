45ae8645f8602a2d2d4f916c02933d495781f59e
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-17 22:48:48 -0500

    Modelo de las notas y servidores

---
+++ Src/Criptografia/MPC/NotaSModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace ProgramacionDiscreta.Src.Criptografia.MPC
+{
+    public class NotaSModel
+    {
+        public int OriginalValue { get; set; }
+
+        public BigInteger Share1 { get; set; }
+
+        public BigInteger Share2 { get; set; }
+
+        public BigInteger Share3 { get; set; }
+    }
+}

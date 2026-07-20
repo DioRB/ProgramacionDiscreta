5a242e7d32b99eb2a8a958e13a5efa77575d7852
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-19 20:34:11 -0500

    Se añadieron los servicios del Dijkstra

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using ProgramacionDiscreta;
 using ProgramacionDiscreta.Src.Criptografia.Cesar;
 using ProgramacionDiscreta.Src.Criptografia.MPC;
 using ProgramacionDiscreta.Src.Criptografia.RSA;
+using ProgramacionDiscreta.Src.Grafos.Dijkstra;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -17,5 +18,7 @@ builder.Services.AddScoped<AritmeticaModularService>();
 builder.Services.AddScoped<RSAService>();
 builder.Services.AddScoped<SecretSharing>();
 builder.Services.AddScoped<MPC>();
+builder.Services.AddScoped<GrafoService>();
+builder.Services.AddScoped<DijkstraService>();
 
 await builder.Build().RunAsync();

1517aa8a0e03ddf1ba0b52b27129a6a4dcde12c1
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-19 20:51:47 -0500

    Agregado el modelo para el resultado del calculo de tramo entre los nodos

---
+++ Src/Grafos/Dijkstra/ResultadoTramoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgramacionDiscreta.Src.Grafos.Dijkstra
+{
+    public class ResultadoTramoModel
+    {
+        public List<string> Path { get; set; } = new();
+
+        public int Distance { get; set; }
+
+        public bool Exists { get; set; }
+    }
+}

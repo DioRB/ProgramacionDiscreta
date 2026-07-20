ff39a5cbed7f16bbbcab3c958e07901b1a9913e8
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-19 20:55:30 -0500

    Modelo que toma las listas de nodos y aristas para la creación de grafos

---
+++ Src/Grafos/Dijkstra/GrafoData.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+namespace ProgramacionDiscreta.Src.Grafos.Dijkstra
+{
+    public class GrafoData
+    {
+        [JsonPropertyName("nodes")]
+        public List<GrafoNodoModel> Nodes { get; set; } = new();
+
+        [JsonPropertyName("edges")]
+        public List<GrafoAristaModel> Edges { get; set; } = new();
+    }
+}

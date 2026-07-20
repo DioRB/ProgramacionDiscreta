ad40635cf56653778c9ce4b32c12f736215d846b
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 15:39:00 -0500

    Modelo para las diferencias de distancia por nodos

---
+++ Src/Grafos/ImpactoRed/ImpactoResultadoModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ProgramacionDiscreta.Src.Grafos.ImpactoRed
+{
+    public class ImpactoResultadoModel
+    {
+        public string Origin { get; set; } = "";
+
+        public string Destination { get; set; } = "";
+
+        public int? DistanceBefore { get; set; }
+
+        public int? DistanceAfter { get; set; }
+
+        public int? Difference { get; set; }
+
+        public string Status { get; set; } = "";
+    }
+}

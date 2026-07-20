7e7043d86bbf24f0de38ae9a3442071e2a0ce9b2
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-19 20:53:32 -0500

    Modelos para la generación de grafos

---
+++ Src/Grafos/Dijkstra/GrafoAristaModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProgramacionDiscreta.Src.Grafos.Dijkstra
+{
+    public class GrafoAristaModel
+    {
+        [JsonPropertyName("from")]
+        public string From { get; set; } = "";
+
+        [JsonPropertyName("to")]
+        public string To { get; set; } = "";
+
+        [JsonPropertyName("weight")]
+        public int Weight { get; set; }
+    }
+}
---
+++ Src/Grafos/Dijkstra/GrafoNodoModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+namespace ProgramacionDiscreta.Src.Grafos.Dijkstra
+{
+    public class GrafoNodoModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = "";
+
+        [JsonPropertyName("x")]
+        public int X { get; set; }
+
+        [JsonPropertyName("y")]
+        public int Y { get; set; }
+    }
+}

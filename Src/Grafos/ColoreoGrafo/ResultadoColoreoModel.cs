2a2f63e260e6f589d24b0664e4c8d7a26ca11dbc
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 20:12:36 -0500

    Modelo para el resultado luego del coloreo

---
+++ Src/Grafos/ColoreoGrafo/ResultadoColoreoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgramacionDiscreta.Src.Grafos.ColoreoGrafo
+{
+    public class ResultadoColoreoModel
+    {
+        public List<AsignaColorModel> Assignments { get; set; } = new();
+        public int NumberOfColors { get; set; }
+        public bool IsValid { get; set; }
+    }
+}

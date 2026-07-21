b4d2eaf9e5c8ae2174095311abb33c4c2cbaf6dc
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 20:11:39 -0500

    Modelo de vertices y color para los nodos

---
+++ Src/Grafos/ColoreoGrafo/AsignaColorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgramacionDiscreta.Src.Grafos.ColoreoGrafo
+{
+    public class AsignaColorModel
+    {
+        public string Vertex { get; set; } = "";
+        public int Color { get; set; }
+    }
+}
+

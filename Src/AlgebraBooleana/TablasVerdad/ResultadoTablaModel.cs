74a8a56b403b66b150a7dde555a2d1b57e3ea5f0
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 20:02:57 -0500

    Modelo para el resultado y expresión booleana

---
+++ Src/AlgebraBooleana/TablasVerdad/ResultadoTablaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgramacionDiscreta.Src.AlgebraBooleana.TablasVerdad
+{
+    public class ResultadoTablaModel
+    {
+        public string ExpressionName { get; set; } = "";
+
+        public List<FilaTablaVerdadModel> Rows { get; set; } = new();
+    }
+}

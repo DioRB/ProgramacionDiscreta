a05d876b8803460e8caee4db3dad1ac1bfda3e86
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 20:01:41 -0500

    Modelo para las entradas/variables

---
+++ Src/AlgebraBooleana/TablasVerdad/EntradaBoolModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProgramacionDiscreta.Src.AlgebraBooleana.TablasVerdad
+{
+    public class EntradaBoolModel
+    {
+        public bool A { get; set; }
+
+        public bool B { get; set; }
+
+        public bool C { get; set; }
+
+        public bool D { get; set; }
+    }
+}

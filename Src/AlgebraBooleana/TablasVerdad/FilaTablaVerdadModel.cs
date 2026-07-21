3214f4ba2b744be0ee024381ba5642fafd789814
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 20:02:11 -0500

    Modelo para los resultados por fila en las tablas

---
+++ Src/AlgebraBooleana/TablasVerdad/FilaTablaVerdadModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ProgramacionDiscreta.Src.AlgebraBooleana.TablasVerdad
+{
+    public class FilaTablaVerdadModel
+    {
+        public bool A { get; set; }
+
+        public bool B { get; set; }
+
+        public bool C { get; set; }
+
+        public bool D { get; set; }
+
+        public bool Result { get; set; }
+    }
+}

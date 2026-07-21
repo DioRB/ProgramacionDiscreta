bca5dcf9393e60b944d203b02ed60861e54cb346
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 20:16:56 -0500

    Servicio que valida el resultado del coloreo del grafo

---
+++ Src/Grafos/ColoreoGrafo/ValidacionColoreoService.cs
@@ -0,0 +1,29 @@
+﻿namespace ProgramacionDiscreta.Src.Grafos.ColoreoGrafo
+{
+    public class ValidacionColoreoService
+    {
+        // Verifica que dos vértices adyacentes nunca tengan el mismo color
+        public bool Validate(
+            Dictionary<string, List<string>> graph,
+            List<AsignaColorModel> assignments)
+        {
+            Dictionary<string, int> colors =
+                assignments.ToDictionary(
+                    a => a.Vertex,
+                    a => a.Color);
+
+            foreach (var vertex in graph.Keys)
+            {
+                foreach (var neighbor in graph[vertex])
+                {
+                    if (colors[vertex] == colors[neighbor])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}

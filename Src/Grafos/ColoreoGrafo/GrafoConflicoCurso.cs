398af7cbe5488f9743a19ca2d3d54cc07eec05bc
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 20:15:12 -0500

    Modelo para el grafo de asignaturas

---
+++ Src/Grafos/ColoreoGrafo/GrafoConflicoCurso.cs
@@ -0,0 +1,10 @@
+﻿using ProgramacionDiscreta.Src.Grafos.Dijkstra;
+
+namespace ProgramacionDiscreta.Src.Grafos.ColoreoGrafo
+{
+    public class GrafoConflicoCurso
+    {
+        public List<GrafoNodoModel> Nodes { get; set; } = new();
+        public List<GrafoAristaModel> Edges { get; set; } = new();
+    }
+}

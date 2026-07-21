16138045968f23d0dfc250193eb754eb19a119fa
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-20 20:04:59 -0500

    Servicio que genera la tabla con los resultados obtenidos del otro servicio

---
+++ Src/AlgebraBooleana/TablasVerdad/GeneradorTablaVerdadService.cs
@@ -0,0 +1,45 @@
+﻿namespace ProgramacionDiscreta.Src.AlgebraBooleana.TablasVerdad
+{
+    public class GeneradorTablaVerdadService
+    {
+        private readonly ExpresionBoolService _expressionService;
+
+        public GeneradorTablaVerdadService(
+            ExpresionBoolService expressionService)
+        {
+            _expressionService = expressionService;
+        }
+
+        public ResultadoTablaModel Generate(int expression)
+        {
+            ResultadoTablaModel table = new();
+
+            table.ExpressionName =
+                _expressionService.GetExpressionName(expression);
+
+            for (int i = 0; i < 16; i++)
+            {
+                bool A = (i & 8) != 0;
+                bool B = (i & 4) != 0;
+                bool C = (i & 2) != 0;
+                bool D = (i & 1) != 0;
+
+                table.Rows.Add(new FilaTablaVerdadModel
+                {
+                    A = A,
+                    B = B,
+                    C = C,
+                    D = D,
+                    Result = _expressionService.Evaluate(
+                        expression,
+                        A,
+                        B,
+                        C,
+                        D)
+                });
+            }
+
+            return table;
+        }
+    }
+}

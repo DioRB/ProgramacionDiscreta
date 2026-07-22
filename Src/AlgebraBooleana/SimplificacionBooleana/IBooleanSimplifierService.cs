d6a3b656e818e2812126c925515d7e26e079713c
Diego Alejandro Robayo cordoba <diegoalrc2006@gmail.com>
2026-07-22 16:00:41 -0500

    Interfaz para usar en el servicio de calculo

---
+++ Src/AlgebraBooleana/SimplificacionBooleana/IBooleanSimplifierService.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgramacionDiscreta.Src.AlgebraBooleana.SimplificacionBooleana
+{
+    public interface IBooleanSimplifierService
+    {
+            /// <summary>
+            /// Simplifica una función booleana de 3 o 4 variables dada por sus minterminos,
+            /// usando el algoritmo Quine-McCluskey, y verifica que la expresión resultante
+            /// tenga la misma tabla de verdad que la original.
+            /// </summary>
+            SimplificationResultModel Simplify(SimplificationRequestModel request);
+        }
+}
